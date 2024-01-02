2275bbb10cc5474d78ac9e12f189e9e60ef3103f
Caio Almeida Santos <aceutoxzc@gmail.com>
2024-01-02 02:02:24 -0300

    algumas funções implementadas

---
+++ javaScript.js
@@ -0,0 +1,22 @@
+function teste(ok1, ok2) {
+    var retorno = ok1 + ok2;
+    document.getElementById("resultado").innerHTML = retorno;
+}
+
+function fibonacci(n) {
+  if (n === 0) {
+    return 0;
+
+  } else if (n === 1) {
+    return 1;
+
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+class Armazem {
+    Armazem(){
+      carnes
+    }
+}
